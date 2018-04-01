d3583a7b56801178b280e4fed1fa2a1117756d6e
Terry <37978100+terryzhang1015@users.noreply.github.com>
2018-04-01 21:55:35 +0800

    finish queue.hpp
    C++ code
    class queue

---
+++ queue.hpp
@@ -0,0 +1,87 @@
+#ifndef QUEUE_HPP
+#define QUEUE_HPP
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+template <class Tp>
+struct Node
+{
+	Tp data;
+	Node *next;
+
+	Node()
+	{
+		next = NULL;
+	}
+};
+
+template <class Tp>
+class queue
+{
+public:
+	queue()
+	{
+		Node <Tp> *p = new Node<Tp>;
+		head = tail = p;
+		s = 0;
+	}
+
+	inline void push(const Tp &x)
+	{
+		Node <Tp> *p = new Node<Tp>;
+
+		p->data = x;
+
+		tail->next = p;
+		tail = p;
+
+		s++;
+	}
+
+	inline void pop()
+	{
+		Node <Tp> *p = head->next;
+
+		head->next = p->next;
+
+		if (tail == p)
+			tail = head;
+		delete p;
+
+		s--;
+	}
+
+	inline Tp front() const
+	{
+		return head->next->data;
+	}
+
+	inline Tp end() const
+	{
+		return tail->data;
+	}
+
+	inline int size() const
+	{
+		return s;
+	}
+
+	inline bool empty() const
+	{
+		return !s;
+	}
+
+	~queue()
+	{
+		delete head, tail;
+	}
+
+protected:
+	Node <Tp> *head, *tail;
+	int s;
+};
+
+#endif
